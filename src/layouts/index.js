95fd28ebded2fa79b37010fe0a9f46a5773f1934
Niall <nialldbarber@gmail.com>
2018-07-21 13:08:17 +0100

    Chaned name of header component

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
-import Header from '../components/header'
+import Header from '../components/Header'
 import favicon from '../../public/static/favicon.ico'
 import './app.sass'
 
